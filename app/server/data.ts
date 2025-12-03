ed8b1190f11e8eca1a349e7c955959d9b9eb4699
Jimmy Angel Pérez Díaz <jianpedi@gmail.com>
2025-12-02 22:56:17 -0500

    fix: load and delete game functions

--- app/server/data.ts
+++ app/server/data.ts
@@ -77,7 +77,12 @@ export const uploadGame = async (params: { name: string; file: File }) => {
   const { name, file } = params;
 
   const gameId = slugify(name).toLowerCase();
-  const gameConsole = file.name.split(".").pop();
+  const gameConsole = file.name?.split(".").pop() ?? undefined;
+
+  if (!gameConsole) {
+    return { success: false, errors: { game_file: "Invalid file name" } };
+  }
+
   const path = `${BASE_PATH}/${gameId}`;
 
   if (await pathExists(path)) {
@@ -104,7 +109,7 @@ export const uploadGame = async (params: { name: string; file: File }) => {
 export const deleteGame = async (gameId: string) => {
   const path = `${BASE_PATH}/${gameId}`;
 
-  await fs.promises.rmdir(path, { recursive: true });
+  await fs.promises.rm(path, { recursive: true, force: true });
 };
 
 export const getRomFile = async (gameId: string, csl: string) => {
